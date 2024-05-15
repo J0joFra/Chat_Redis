9b7f0a314109c88368a842f928fc97f33276915d
Giulio Salaorni <giulio.salaorni@itsrizzoli.it>
2024-05-15 12:36:20 +0200

    problemi in arrivo...

--- NumeriHundred.py
+++ NumeriHundred.py
@@ -1,3 +1,5 @@
 #Scrivere un codice in Python che stampi i numeri da 1 a 100
-for number in range(100):
+MIN = int(input("Inserire numero da cui partire: "))
+MAX = int(input("Inserire numero a cui arrivare: "))
+for number in range(MIN,MAX + 1):
     print(number)
