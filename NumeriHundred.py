df86fe2b4e833a939916349c65b457b0ac8549db
Joaquim Francalanci <joaquim.francalanci@itsrizzoli.it>
2024-05-15 12:34:04 +0200

    Big Jo

--- NumeriHundred.py
+++ NumeriHundred.py
@@ -1,3 +1,13 @@
 #Scrivere un codice in Python che stampi i numeri da 1 a 100
+somma = 0
 for number in range(100):
-    print(number)
+    if number % 7 == 0 and number != 0:
+        number += 1
+        print("Buzz")
+    else:
+        somma += number
+        print(number)
+print(f"La somma dei numeri è {somma}")
+
+
+
